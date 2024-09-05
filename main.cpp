90c2c9a1df18596c0bc12a9b9bca30b1a4c83828
juuubsbs <julianasimon@edu.univali.br>
2024-09-05 08:57:34 -0300

    nada de funcionar essas setinhas feias

--- main.cpp
+++ main.cpp
@@ -8,11 +8,46 @@
     gerar um relatório diário ou relatório de uso*/
 
 #include <iostream>
-#include <cadastro.hpp> // para cadastrar os produtos, fazer com struct
-#include <venda.hpp>
+#include <windows.h>
+
+
+
 
 
 int main(){
+
+    //início do menu
+    std:: string cadastro, venda, pagamento, sair; //textos exibidos no menu
+    cadastro = "------------Cadastro-------------";
+    venda    = "--------------Venda--------------";
+    pagamento= "------------Pagamento------------";
+    sair     = "--------------Sair---------------";
+
+    std:: cout << cadastro << std::endl;
+    std:: cout << venda << std:: endl;
+    std:: cout << pagamento << std:: endl;
+    std:: cout << sair << std:: endl;
+
+    int seta; 
+    seta = GetAsyncKeyState(VK_UP);
+    std:: cin.get() >> seta;
+
+        switch (seta){
+        case 72:
+            std:: cout << "oie";
+            break;
+        default:
+            std:: cout << "vish";
+        }
+
+
+
+
+
+
+
+
+                  
 
     return 0;
 }
